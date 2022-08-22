db36f3349a0384319699ade9895c4526078fda50
TomShiDi <1341109792@qq.com>
2022-08-22 20:25:55 +0800

    原子性演示代码

--- src/com/demo/volatiles/VolatileDemo.java
+++ src/com/demo/volatiles/VolatileDemo.java
@@ -2,14 +2,37 @@ package com.demo.volatiles;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class VolatileDemo {
 
     public static void main(String[] args) {
-
+        atomic();
     }
 
+    public static void atomic() {
+        Data data = new Data();
+        CountDownLatch countDownLatch = new CountDownLatch(20);
+        for (int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                for (int j = 1; j <= 1000; j++) {
+                    data.addByPreValueAtomic(1);
+                }
+                countDownLatch.countDown();
+            }, "thread" + i).start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("【%s】：主线程获取到的最终number值：%s\n", Thread.currentThread(), data.atomicNumber.get());
+    }
+
+    /**
+     * volatile不能保证原子性
+     */
     public static void volatileCannotEnsureAtomic() {
         Data data = new Data();
         CountDownLatch countDownLatch = new CountDownLatch(20);
@@ -56,6 +79,8 @@ public class VolatileDemo {
     static class Data {
         volatile int number = 0;
 
+        AtomicInteger atomicNumber = new AtomicInteger(0);
+
         public void addTo60() {
             this.number = 60;
         }
@@ -63,6 +88,17 @@ public class VolatileDemo {
         public void addByPreValue() {
             this.number++;
 //            this.number = this.number + 1;
+        }
+
+        public void addByPreValueAtomic() {
+            atomicNumber.incrementAndGet();
+        }
+
+        public void addByPreValueAtomic(int addNumber) {
+            int currValue = 0;
+            do {
+                currValue = this.atomicNumber.get();
+            } while (!this.atomicNumber.compareAndSet(currValue, currValue + addNumber));
         }
     }
 }
