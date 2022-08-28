5580f1630f710bc6145cb753a0f9ad647019ab4a
TomShiDi <1341109792@qq.com>
2022-08-28 14:41:52 +0800

    读写锁组合场景

--- src/com/demo/readwritelock/ReadWriteLockMain.java
+++ src/com/demo/readwritelock/ReadWriteLockMain.java
@@ -7,6 +7,10 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
+ * 见JUC并发编程笔记
+ * 读-读 兼容
+ * 读-写 互斥
+ * 写-写 互斥
  * @author TomShiDi
  * @description
  * @date 2021/10/5 17:40
@@ -14,7 +18,10 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class ReadWriteLockMain {
     public static void main(String[] args) throws InterruptedException {
         AtomicOperation atomicOperation = new AtomicOperation();
+        new Thread(() -> atomicOperation.atomicPut("1", 1), "PUT1").start();
         new Thread(() -> atomicOperation.atomicGet("1"),"GET1").start();
+        // 读锁为共享锁，多个读锁线程可以并行
+        new Thread(() -> atomicOperation.atomicGet("1"),"GET2").start();
         TimeUnit.SECONDS.sleep(3);
         new Thread(() -> atomicOperation.atomicPut("1", 1), "PUT1").start();
     }
@@ -24,9 +31,14 @@ public class ReadWriteLockMain {
         private Map<String, Integer> dataMap = new HashMap<>(8);
 
         public void atomicPut(String key, Integer value) {
-            System.out.printf("【%s】：获取【写锁前】\n", Thread.currentThread().getName());
+            System.out.printf("【%s】：准备获取【写锁】\n", Thread.currentThread().getName());
             readWriteLock.writeLock().lock();
             System.out.printf("【%s】：获取【写锁后】\n", Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             try {
                 dataMap.put(key, value);
             } catch (Exception e) {
@@ -38,7 +50,7 @@ public class ReadWriteLockMain {
         }
 
         public Integer atomicGet(String key) {
-            System.out.printf("【%s】：获取【读锁前】\n", Thread.currentThread().getName());
+            System.out.printf("【%s】：准备获取【读锁】\n", Thread.currentThread().getName());
             readWriteLock.readLock().lock();
             System.out.printf("【%s】：获取【读锁后】\n", Thread.currentThread().getName());
             Integer result = null;
