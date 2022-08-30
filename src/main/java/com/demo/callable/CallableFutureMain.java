8dd038eac6c0739c974b702b9540f85936b0c4cd
TomShiDi <1341109792@qq.com>
2022-08-30 21:27:38 +0800

    callable使用Demo增加注释

--- src/main/java/com/demo/callable/CallableFutureMain.java
+++ src/main/java/com/demo/callable/CallableFutureMain.java
@@ -1,5 +1,8 @@
 package com.demo.callable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
@@ -11,13 +14,18 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @date 2021/10/1 20:55
  **/
 public class CallableFutureMain {
+    private final static Logger LOGGER = LoggerFactory.getLogger(CallableFutureMain.class);
     private static AtomicInteger count = new AtomicInteger(0);
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         FutureTask<Integer> futureTask = new FutureTask<>(() -> {
+            LOGGER.info("进入callable方法");
             TimeUnit.SECONDS.sleep(5);
             return count.incrementAndGet();
         });
         new Thread(futureTask, "A").start();
+        // 对于同一个FutureTask对象，即使被多个线程引用，也只会执行一次
+        // 原因在于FutureTask的run方法中做了如下判断：1. 是否已被执行过；2. 是否被其他线程持有
+        new Thread(futureTask, "B").start();
         while (!futureTask.isDone()) {
             System.out.printf("%s：任务还未完成\n", Thread.currentThread().getName());
             TimeUnit.MICROSECONDS.sleep(200);
